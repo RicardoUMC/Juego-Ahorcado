2a6bd3d36f8a26f0fb0004717a84e2db64ae1d94
Ricardo Mora <rurimoca@gmail.com>
2020-05-06 20:11:02 -0500

    Se comentó una reservación de memoria, se va a probar otra forma

--- ahorcado.c
+++ ahorcado.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
+  // int *letter;
+  // letter = (char*) malloc(10 * sizeof(char));
+  // if (letter == NULL) {
+  //   printf("No se pudo reservar el espacio de memoria");
+  //   exit(1);
+  // }
+
   char array[5] = "kirby";
-  printf("Hola\n");
+
+  printf("Juguemos ahorcado, tienes 10 intentos.\n");
+
+  printf("Prueba con una letra: ");
+  scanf("%s\n", &letter);
+
+
 
   return 0;
 }
